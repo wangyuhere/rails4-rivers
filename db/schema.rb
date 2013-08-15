e14b5556967f68aac72fd950b6af991ef266e919
Martin Svalin and Yu Wang <dev+martin+yu@mynewsdesk.com>
2013-08-15 09:51:16 +0200

    using postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20130808203250) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "pearls", force: true do |t|
     t.integer  "river_id"
     t.string   "url"
